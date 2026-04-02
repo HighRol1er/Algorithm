762aa28e6bdad2fd194f20025769daea90488a01
HighRol1er <158163589+HighRol1er@users.noreply.github.com>
2026-04-02 19:09:01 +0900

    [Bronze I] Title: 이항 계수 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,6 +1,7 @@
 N,K = map(int, input().split())
 
-# 이항 계수? 
+# 이항 계수? -> 주어진 집합에서 순서에 상관없이 일정 개수의 원소를 선택하는 방법의 가짓수 
+#  $n$개 중에서 $k$개를 뽑는 경우의 수"**인 **조합(Combination)**과 같은 개념
 
 def factorial(x):
   result = 1
@@ -9,5 +10,3 @@ def factorial(x):
   return result
 
 print( factorial(N) // (factorial(K) * factorial(N-K)))
-
-  
