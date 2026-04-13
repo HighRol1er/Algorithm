4497923bd8207d7ef2bdce6574bbbdbb848f3641
HighRol1er <158163589+HighRol1er@users.noreply.github.com>
2026-04-13 19:51:34 +0900

    [Silver II] Title: 색종이 만들기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,32 +1,37 @@
 n = int(input())
 
-m = []
+matrix = []
 
 for _ in range(n):
   l = list(map(int, input().split()))
-  m.append(l)
+  matrix.append(l)
 
-blue = 0   #1
-white = 0  #0
+blue = 0
+white = 0 
 
-def v(row, col, size):
-  global blue, white      
-  current = m[row][col]
+def recursion(row, col, size):
+  global blue,white
+  current = matrix[row][col] # 0 or 1
+
   for r in range(row, row + size):
     for c in range(col, col + size):
-      if m[r][c] != current:
-       half = size // 2
-       v(row, col, half)
-       v(row, col + half, half)
-       v(row + half, col, half)
-       v(row + half, col + half, half)
-       return
-  
+      if matrix[r][c] != current:
+        half = size // 2
+        #I
+        recursion(row, col, half)
+        # II
+        recursion(row + half, col, half)
+        # III
+        recursion(row, col + half, half)
+        # IV 
+        recursion(row+ half, col + half, half)
+        return
+      
   if current == 0:
     white +=1 
   else:
-    blue += 1 
-
-v(0,0,n)
+    blue += 1
+  
+recursion(0,0,n)
 print(white)
 print(blue)
